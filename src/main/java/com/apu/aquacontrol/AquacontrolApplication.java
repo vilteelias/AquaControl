3b664ce759378f65748446f7a295c20dfa9973c5
vilteelias <vilteelias5@gmail.com>
2026-07-07 17:58:17 -0300

    Initialize Spring Boot project

---
+++ src/main/java/com/apu/aquacontrol/AquacontrolApplication.java
@@ -0,0 +1,13 @@
+package com.apu.aquacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquacontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquacontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apu/aquacontrol/AquacontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apu.aquacontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquacontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
