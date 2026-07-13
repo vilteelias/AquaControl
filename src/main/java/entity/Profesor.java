9bbedb810c91487259f72d8b71485a43b9b18e74
vilteelias <vilteelias5@gmail.com>
2026-07-13 04:45:57 -0300

    feature: Crear entidad Profesor con mapeo JPA

---
+++ src/main/java/entity/Profesor.java
@@ -0,0 +1,67 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="profesores")
+
+public class Profesor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(nullable = false, length = 100)
+    private String apellido;
+
+    @Column(nullable = false, unique = true, length = 10)
+    private int dni;
+
+    //El constructor vacío es obligatorio para JPA/Hibernate
+    public Profesor() {
+    }
+
+    public Profesor(String nombre, String apellido, int dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    //Getters y Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+
+
+}
